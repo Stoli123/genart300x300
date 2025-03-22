d89faba6bfb2238c6963e557ac50a32e992ab204
Stoli123 <61210224+Stoli123@users.noreply.github.com>
2025-03-22 16:55:24 -0400

    Update generate_plot.R

--- R/generate_plot.R
+++ R/generate_plot.R
@@ -31,7 +31,7 @@ generate_plot <- function(df, file_name, polar, filetype, color = "black", backg
         )
   } else {
     plot <- df %>%
-      ggplot2::ggplot(ggplot2::aes(x = x, y = y)) +
+      ggplot2::ggplot(ggplot2::aes(x = x/2, y = y/2)) +
       ggplot2::geom_point(alpha = 0.1, size = 0, shape = 20, color = color) +
       ggplot2::theme_void() +
       ggplot2::coord_fixed() +
